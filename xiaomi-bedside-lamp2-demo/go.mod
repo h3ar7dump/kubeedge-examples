3d4cd351616b6cf05b0efcbd0ca936f07c7052dc
h3ar7dump <awenluo@outlook.com>
2020-07-22 15:57:13 +0800

    [ADD]init commit,add all code

---
+++ xiaomi-bedside-lamp2-demo/go.mod
@@ -0,0 +1,10 @@
+module lamp2
+
+go 1.14
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	gopkg.in/yaml.v2 v2.3.0
+)
---
+++ xiaomi-bedside-lamp2-demo/lamp-driver/lamp_driver.go
@@ -0,0 +1,55 @@
+package lamp_driver
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func TurnON() {
+	url := "http://192.168.31.193:8123/api/services/light/turn_on"
+	method := "POST"
+
+	payload := strings.NewReader("{\"entity_id\": \"light.xiaomi_bedside_lamp2\"}")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, strings.TrimSpace(url), payload)
+
+	if err != nil {
+		fmt.Println("-------------------")
+		fmt.Println(err)
+		fmt.Println("-------------------")
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2Y2Q4ZjM3Yjc4MDU0M2E5YWEyZDUyZDQyMGE1Y2NhOSIsImlhdCI6MTU5MjM3Mjk1OCwiZXhwIjoxOTA3NzMyOTU4fQ.sq_LrBFOhfxmxOowC1-HcZKw3YzCF1wYzbvV61extiw")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+}
+
+func TurnOff() {
+	url := "http://192.168.31.193:8123/api/services/light/turn_off"
+	method := "POST"
+
+	payload := strings.NewReader("{\"entity_id\": \"light.xiaomi_bedside_lamp2\"}")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2Y2Q4ZjM3Yjc4MDU0M2E5YWEyZDUyZDQyMGE1Y2NhOSIsImlhdCI6MTU5MjM3Mjk1OCwiZXhwIjoxOTA3NzMyOTU4fQ.sq_LrBFOhfxmxOowC1-HcZKw3YzCF1wYzbvV61extiw")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+
+}
