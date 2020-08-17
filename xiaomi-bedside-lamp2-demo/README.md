3505d13eb125203f952f6b5c00fe9df916088855
h3ar7dump <awenluo@outlook.com>
2020-08-17 18:01:24 +0800

    <UPDATE> update bsl device mapper

--- xiaomi-bedside-lamp2-demo/README.md
+++ xiaomi-bedside-lamp2-demo/README.md
@@ -1,4 +1,6 @@
 ```
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build light-mapper.go
-docker build -t lamp-mapper:v1.2 . 
+docker build -t lamp-mapper:v1.2.1 . 
+docker tag lamp-mapper:v1.2.1 kindlaw/lamp-mapper:v1.2.1
+docker push kindlaw/lamp-mapper:v1.2.1
 ```
--- xiaomi-bedside-lamp2-demo/lamp-driver/lamp_driver.go
+++ xiaomi-bedside-lamp2-demo/lamp-driver/lamp_driver.go
@@ -22,7 +22,7 @@ func TurnON() {
 		fmt.Println("-------------------")
 	}
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2Y2Q4ZjM3Yjc4MDU0M2E5YWEyZDUyZDQyMGE1Y2NhOSIsImlhdCI6MTU5MjM3Mjk1OCwiZXhwIjoxOTA3NzMyOTU4fQ.sq_LrBFOhfxmxOowC1-HcZKw3YzCF1wYzbvV61extiw")
+	req.Header.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYzEwN2FmMTZiM2U0MjM4YmMyNzZlMGQ3NGE4ZTIyMSIsImlhdCI6MTU5NzY1MjM4NywiZXhwIjoxOTEzMDEyMzg3fQ.NWUwCrLZQmLG0z0GT1BXddr8O1AsVoVsviQLSubuOiw")
 
 	res, err := client.Do(req)
 	defer res.Body.Close()
