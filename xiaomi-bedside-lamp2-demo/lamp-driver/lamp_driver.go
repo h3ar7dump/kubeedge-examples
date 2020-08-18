aee936d1eb9db78c258e456b14c128c95b1904ab
h3ar7dump <awenluo@outlook.com>
2020-08-18 14:48:26 +0800

    <UPDATE> update test token

--- xiaomi-bedside-lamp2-demo/lamp-driver/lamp_driver.go
+++ xiaomi-bedside-lamp2-demo/lamp-driver/lamp_driver.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+const bearerToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYzEwN2FmMTZiM2U0MjM4YmMyNzZlMGQ3NGE4ZTIyMSIsImlhdCI6MTU5NzY1MjM4NywiZXhwIjoxOTEzMDEyMzg3fQ.NWUwCrLZQmLG0z0GT1BXddr8O1AsVoVsviQLSubuOiw"
+
 func TurnON() {
 	url := "http://192.168.31.193:8123/api/services/light/turn_on"
 	method := "POST"
@@ -22,7 +24,7 @@ func TurnON() {
 		fmt.Println("-------------------")
 	}
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjYzEwN2FmMTZiM2U0MjM4YmMyNzZlMGQ3NGE4ZTIyMSIsImlhdCI6MTU5NzY1MjM4NywiZXhwIjoxOTEzMDEyMzg3fQ.NWUwCrLZQmLG0z0GT1BXddr8O1AsVoVsviQLSubuOiw")
+	req.Header.Add("Authorization", bearerToken)
 
 	res, err := client.Do(req)
 	defer res.Body.Close()
@@ -44,7 +46,7 @@ func TurnOff() {
 		fmt.Println(err)
 	}
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI2Y2Q4ZjM3Yjc4MDU0M2E5YWEyZDUyZDQyMGE1Y2NhOSIsImlhdCI6MTU5MjM3Mjk1OCwiZXhwIjoxOTA3NzMyOTU4fQ.sq_LrBFOhfxmxOowC1-HcZKw3YzCF1wYzbvV61extiw")
+	req.Header.Add("Authorization", bearerToken)
 
 	res, err := client.Do(req)
 	defer res.Body.Close()
